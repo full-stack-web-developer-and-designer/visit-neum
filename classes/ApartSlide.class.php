0819a5bec9ef94127ab638c920834fbe01946e39
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2021-03-02 18:42:25 +0100

    Add slider for Hotel & Apart

---
+++ classes/ApartSlide.class.php
@@ -0,0 +1,12 @@
+<?php
+class ApartSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM visitneu_slider.lightslider WHERE category='Apartments' ORDER BY rand();";
+        $stmt = $this->connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+            echo "<li class='item-a'><article class='offer'><a href='$row[href]'><img src='./images/$row[img]' alt='$row[alt]'/><span><h2>$row[title]</h2></span></a></article></li>";
+            }
+        }
+    }
