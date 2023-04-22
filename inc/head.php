e6622c1d16f7b2c8ef37ea316408c7bc23f7d53c
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-04-22 04:23:32 +0200

    Added google analytics

--- inc/head.php
+++ inc/head.php
@@ -118,11 +118,11 @@ window.onorientationchange = function() {
 <title><?php echo $title; ?></title>
 </head>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EMXDHHJCXL"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B5G3QT9BQ7"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-EMXDHHJCXL');
+  gtag('config', 'G-B5G3QT9BQ7');
 </script>
