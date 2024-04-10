09c895e6d4e7c7454e7f54405afac93288501d6c
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-10 23:13:03 +0900

    Removed my phone number from slogan

--- classes/Header.class.php
+++ classes/Header.class.php
@@ -4,6 +4,6 @@ class Header extends Entity{
     public static $tableName = 'header';
     public static $keyColumn = 'header_id';
     public function render() {
-        echo '<div id="logo"><a href="' . $this->header_href .'"><img src="/images/'. $this->header_img .'" width="250px" height="40px" alt="'. $this->header_alt .'" title="'. $this->header_title .'"></a></div><div id="slogan"><p>'. $this->header_slogan .'<br><span id="number"><a href="tel:".$this->header_tel\"">'.$this->header_tel .'</span></p></div>';
+        echo '<div id="logo"><a href="' . $this->header_href .'"><img src="/images/'. $this->header_img .'" width="250px" height="40px" alt="'. $this->header_alt .'" title="'. $this->header_title .'"></a></div><div id="slogan"><p>'. $this->header_slogan .'<br><span id="number">DIREKTNO</span></p></div>';
     }
 }
