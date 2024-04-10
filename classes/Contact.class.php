28e1a99d8e51c84eb231657ec22a5e7b9cae1dda
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-10 23:31:21 +0900

    Good written code

--- classes/Contact.class.php
+++ classes/Contact.class.php
@@ -5,36 +5,37 @@
         
         public function render(){
             $render = "
-<table id='contact'>
-      <thead>
-        <tr>
-          <th colspan='2'><h3>KONTAKT</h3><span id='website'><a href=\"$this->website\" target='_blank'>$this->website</a></span></th>
-        </tr>
-		  </thead>
+                <table id='contact'>
+                    <thead>
+                        <tr>
+                            <th colspan='2'><h3>KONTAKT</h3><span id='website'><a href=\"$this->website\" target='_blank'>$this->website</a></span></th>
+                        </tr>
+                    </thead>
 
-		<tbody>
-        <tr>
-          <td class='table_information'>Vlasnik</td>
-          <td class='table_answer'>$this->vlasnik</td>
-        </tr>
-        <tr>
-          <td class='table_information'>Telefon:</td>
-			<td><a href='tel:$this->tel'>$this->tel</a></td>
-        </tr>
-        <tr>
-          <td class='table_information'>Adresa:</td>
-          <td>$this->adresa</td>
-        </tr>
-        </tbody>
-		<tfoot>
-        <tr>
-        	<td colspan='2'><p id='price'>$this->price</p>
-          <a href='#contact_owner' rel=\"modal:open\"><button id='question' class='request  modal-btn'>$this->question</button></a>
-          
-			</td>
-       </tr>
-      </tfoot>
-    </table>";
-    return $render;
+                    <tbody>
+                        <tr>
+                            <td class='table_information'>Vlasnik</td>
+                            <td class='table_answer'>$this->vlasnik</td>
+                        </tr>
+                        <tr>
+                            <td class='table_information'>Telefon:</td>
+                            <td><a href='tel:$this->tel'>$this->tel</a></td>
+                        </tr>
+                        <tr>
+                            <td class='table_information'>Adresa:</td>
+                            <td>$this->adresa</td>
+                        </tr>
+                    </tbody>
+                        
+                    <tfoot>
+                        <tr>
+                            <td colspan='2'><p id='price'>$this->price</p>
+                                <a href='#contact_owner' rel=\"modal:open\"><button id='question' class='request  modal-btn'>$this->question</button></a>
+                            </td>
+                            </tr>
+                    </tfoot>
+                </table>";
+
+            return $render;
         }
     }
