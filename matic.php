d80b664265de0dfd4ca7c3f082991aebce982087
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-11 23:29:27 +0900

    Removed unnecessary comments

--- matic.php
+++ matic.php
@@ -9,19 +9,14 @@
 <html lang="bs-BA">
 <?php	
 	$title="Hotel Villa Matić, Neum | Najniže cijene smještaja!Posjetite odmah!";
-    //Check length of title for SEO (50-70 characters)
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
 	$og_title="Hotel Villa Matić, Neum | Najpovoljnije cijene smještaja!";
-	$website_url="visit-neum.com/matic.php";
+	$website_url="https://visit-neum.com/Hotel-Villa-Matic.html";
 	$meta['description']="Hotel Villa MATIĆ nudi usluge smještaja u 50 luksuzno opremljenih smještajnih jedinica, kao i restoran, recepciju, parking, garažu...prekrasan pogled na jadransko more.";
-    //Check length of description for SEO (140-180 characters)
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
 	$meta['keywords']="neum, hoteli, Hoteli Neum, apartmani, sobe, ljetovanje,  matić, matic, vila matić, pansioni, privatni smještaj, smjestaj, ljetovanje neum, neum apartmani i sobe";
 	$og_image="./gallery/HotelVillaMatic.jpg";
   $twitter_image="./gallery/HotelVillaMatic.webp";
 	$image_alt="Hotel Villa Matić - Neum";
 	include './inc/head.php';
-	//end #head
 ?>
 <body>
 <div id="wrapper">
@@ -29,9 +24,9 @@
 <?php 
   echo $header->render(); 	
 ?>
-</header><!-- end #header -->
+</header>
 <?php
-  include './inc/nav.inc'; //end #nav
+  include './inc/nav.inc';
 ?>
 <div id="main">
 <?php echo $page->render(); ?> 
@@ -44,7 +39,7 @@
 ?>
 
 <h2>Gdje se nalazimo?</h2>
-<iframe id="map_matic" class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2636.4830002748577!2d17.624211315172573!3d42.91890390761864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134ba2520e6e6c61%3A0xcc90d46d336c0d38!2sVilla+Mati%C4%87!5e1!3m2!1sbs!2sba!4v1519715607404" frameborder="0" allowfullscreen title="Hotel Villa Matić Map"></iframe></div><!-- end #main -->
+<iframe id="map_matic" class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2636.4830002748577!2d17.624211315172573!3d42.91890390761864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134ba2520e6e6c61%3A0xcc90d46d336c0d38!2sVilla+Mati%C4%87!5e1!3m2!1sbs!2sba!4v1519715607404" frameborder="0" allowfullscreen title="Hotel Villa Matić Map" loading="lazy"></iframe></div><!-- end #main -->
 <aside id="sidebar">
 <!-- contact info -->
 <?php echo $contact->render(); ?>
@@ -53,10 +48,9 @@
 	$maticAsside = new GalleryAssideMatic();
 	$maticAsside->renderGalleryAsside();
 ?>
-</aside>
+</aside><!-- end #sidebar -->
 <?php
 	include 'inc/footer.php';
-	//end #footer
 ?>
 </div><!-- end #wrapper -->
 <script src="lightbox2/src/js/lightbox.min.js"></script>
