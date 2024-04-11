1d9fc281d7cc3901327e3ff4e40dea1bc8ffed47
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-11 13:27:58 +0900

    Added og_image, twitter_image, image_alt and CSS for main and sidebar

--- rent_a_car.php
+++ rent_a_car.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'core/init.php';
-$postArr = Header::get(1)
+$header = Header::get(1)
 ?>
 <!doctype html>
 <html lang="bs-BA">
@@ -10,39 +10,42 @@ $postArr = Header::get(1)
     //echo "Length of title is " . strlen($title) . "characters long." . " ";
 	$meta['description']="Najbolja i najudobnija vozila u Neumu, Bosna i Hercegovina – pronađite i rezervišite rent-a-car vozilo po svome ukusu";
     //Check length of description for SEO (140-180 characters)
-    echo "Length of description is " . strlen($meta['description']) . "characters long.";
+    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
 	$meta['keywords']="neum, rent-a-car, najam vozila u neumu, ljetovanje,  ljetovanje neum, neum odmor, vožnja, automobili";
+	$website_url="https://visit-neum.com";
+	$og_image="./gallery/visit-neum.jpg";
+	$twitter_image="./gallery/visit-neum.webp";
+	$image_alt="logo | visit-neum.com";
 	include 'inc/head.php';
 ?>
 <style>
-#main p{text-align: center;}
-#sidebar{background: #FFF;}
-</style><!-- end #head -->
-
+	.accomodation #main {margin: 0 0 10px 0;}
+	#main p{text-align: center;}
+	#sidebar{background: #FFF;}
+</style>
 <body class = "accomodation">
 <div id="wrapper">
-
 <header id="header" class="cf">
 <?php 
-  echo $postArr->render(); 	
-  include 'inc/header.php'; 
+  echo $header->render();
 ?>
 </header><!-- end #header -->
 <?php
   include 'inc/nav.inc'; //end #nav
 ?>
 	<div id="main">
-<p>Uskoro...</p>
+		<p>Uskoro...</p>
 	</div>
 <aside id="sidebar">
 	
 </aside>
 
- <script src="lightbox2/src/js/lightbox.js"></script>
+
 <?php
 	include 'inc/footer.php';
 	//end #footer
 ?>
 </div><!-- end #wrapper -->
+<script src="lightbox2/src/js/lightbox.min.js"></script>
 </body>
 </html>
