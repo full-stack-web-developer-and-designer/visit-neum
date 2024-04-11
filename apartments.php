f7ca26996829c7edacbeb0fc68e14b1756092675
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-12 00:46:55 +0900

    Removed unnecessary comments

--- apartments.php
+++ apartments.php
@@ -7,11 +7,7 @@ $categories = Categories::get(2);
 <html lang="bs-BA">
 <?php	
 	$title="Najbolji Apartmani u Neumu! | Najniže cijene smještaja!";
-    //Check length of title for SEO (50-70 characters) - 57
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
 	$meta['description']="Pronađite najbolje, najpovoljnije i najudobnije apartmane u Neumu! Direktan kontakt s vlasnicima! Prekrasni apartmani s pogledom na more! Najpovoljnije cijene smještaja! Posjetite odmah!";
-    //Check length of description for SEO (140-180 characters)-188
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
 	$meta['keywords']="neum, apartmani, najbolji apartmani u neumu, najjeftiniji apartmani u Neumu, apartmani mampas, apartmani liberan, sobe, ljetovanje, hoteli, pansioni, privatni smještaj, smjestaj, ljetovanje neum, neum apartmani i sobe, neum aparmani cijene";
 	$website_url="https://visit-neum.com/the-best-apartments-in-neum.html";
 	$og_image="./gallery/visit-neum.jpg";
@@ -21,37 +17,29 @@ $categories = Categories::get(2);
 	//end #head
 ?>
 <body class = "accomodation">
-<div id="wrapper">
-<header id="header" class="cf">
-<?php 
-  echo $header->render(); 	
-?>
-</header><!-- end #header -->
-<?php
-  include 'inc/nav.inc'; //end #nav
-?>
-	
-<div id="main">
-<?php echo $categories->render(); ?> 
-<section class="offers">
-	<!-- slider box -->
-	<?php
-	$apart = new ApartSlide();
-	$apart->renderSlider();
-	?>
-</section><!--end .offers-->
-</div><!-- end #main -->
-<aside id="sidebar"> 
-	<?php
-       include 'inc/weather.inc';
-	?>
-</aside><!-- end #sidebar -->
-  
-<?php 
-	include 'inc/footer.php'
-	//end #footer
-?>
+	<div id="wrapper">
+		<header id="header" class="cf">
+			<?php echo $header->render(); ?>
+		</header>
+		
+		<?php include 'inc/nav.inc'; ?>
+			
+		<div id="main">
+			<?php echo $categories->render(); ?>
+				<section class="offers">
+					<!-- slider box -->
+					<?php
+					$apart = new ApartSlide();
+					$apart->renderSlider();
+					?>
+				</section><!--end .offers-->
+		</div><!-- end #main -->
 
-</div><!-- end #wrapper -->
+		<aside id="sidebar">
+			<?php include 'inc/weather.inc'; ?>
+		</aside><!-- end #sidebar -->
+
+		<?php include 'inc/footer.php' ?>
+	</div><!-- end #wrapper -->
 </body>
 </html>
