c20b3e17bf43aa97ef0daa72a23a7d2890a81b58
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-13 02:36:07 +0900

    Removed unnecessary PHP class

--- classes/BrodHeader.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class BrodHeader extends Entity{
-    public static $tableName = 'header';
-    public static $keyColumn = 'header_id';
-    public function render() {
-        echo '<div id="logo"><a href="' . $this->header_href .'"><img src="/images/'. $this->header_img .'" width="250px" height="40px" alt="'. $this->header_alt .'" title="'. $this->header_title .'"></a></div><div id="slogan"><p>'. $this->header_slogan .'<br><span id="number"><a href="tel:+38763350230">'.$this->header_tel .'</span></p></div>';
-    }
-}
