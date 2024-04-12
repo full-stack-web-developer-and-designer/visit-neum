687e869b79b414341bd612be150eb4d975e55917
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-13 02:45:32 +0900

    Added twitter_image for error page and made order of code nicely

--- 404.php
+++ 404.php
@@ -1,42 +1,46 @@
 <?php
 	require_once 'core/init.php';
-$postArr = Header::get(2);
+	$header = Header::get(1);
 ?>
 <!DOCTYPE html>
 <html lang="bs-BA">
-<?php	
-	$title="Gre&scaron;ka";
-	$og_title="Gre&scaron;ka";
-	$website_url="www.visit-neum.com/";
-	$meta['description']="Tra&zcaron;ena stranica ne postoji!";
-	$meta['keywords']="";
-	$og_image="images/error.404.png";
-	$image_alt="Gre&scaron;ka!";
-	include 'inc/head.php';	
-?>
-<style>
-.accomodation #main {margin: 0 0 10px 0;}
-#main p{text-align: center;}
-#sidebar{background: #FFF;}
-</style><!-- end #head -->
+	<?php	
+		$title="Gre&scaron;ka";
+		$og_title="Gre&scaron;ka";
+		$website_url="https://visit-neum.com/error404.html";
+		$meta['description']="Tra&zcaron;ena stranica ne postoji!";
+		$meta['keywords']="";
+		$og_image="images/error.404.png";
+		$twitter_image="images/error.404.png";
+		$image_alt="Gre&scaron;ka!";
+		include 'inc/head.php';	
+	?>
+	<style>
+		.accomodation #main {margin: 0 0 10px 0;}
+		#main p{text-align: center;}
+		#sidebar{background: #FFF;}
+	</style><!-- end #head -->
 
-<body class="accomodation">
-<div id="wrapper">
-<header id="header" class="cf">
-<?php 
-  echo $postArr->render(); 	
-  include 'inc/header.php'; 
-?>
-</header><!-- end #header -->
-<?php
-  include 'inc/nav.inc'; //end #nav
-?>
-<div id="main"><p><b><i>"Tra&zcaron;ena stranica ne postoji! Provjerite da li ste dobro unijeli adresu!"</i></b></p></div>
-<aside id="sidebar"><?php include 'inc/weather.inc'; ?></aside><!-- end #sidebar --> 
-<?php
-	include 'inc/footer.php';
-	//end #footer
-?>
-</div><!-- end #wrapper -->
-</body>
+	<body class="accomodation">
+		<div id="wrapper">
+			<header id="header" class="cf">
+			
+				<?php echo $header->render(); ?>
+
+			</header>
+
+			<?php include 'inc/nav.inc'; ?>
+			
+			<div id="main">
+				<p><b><i>"Tra&zcaron;ena stranica ne postoji! Provjerite da li ste dobro unijeli adresu!"</i></b></p>
+			</div>
+
+			<aside id="sidebar">
+				<?php include 'inc/weather.inc'; ?>
+			</aside>
+			
+			<?php include 'inc/footer.php'; ?>
+			
+		</div><!-- end #wrapper -->
+	</body>
 </html>
