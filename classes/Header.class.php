ade76f652894f6e25689a100f5f85ac199d434b0
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-21 01:22:07 +0900

    Stop tracking files for classes

--- classes/Header.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Header extends Entity{
-    public static $tableName = 'header';
-    public static $keyColumn = 'header_id';
-    public function render() {
-        echo '<div id="logo"><a href="' . $this->header_href .'"><img src="/images/'. $this->header_img .'" width="250px" height="40px" alt="'. $this->header_alt .'" title="'. $this->header_title .'"></a></div><div id="slogan"><p>'. $this->header_slogan .'<br><span id="number">DIREKTNO</span></p></div>';
-    }
-}
