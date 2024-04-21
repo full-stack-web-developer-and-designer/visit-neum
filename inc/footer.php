f27365a7a2921878d989bdc185e9e88aba9a0df7
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-21 22:48:40 +0900

    Added &copy for copyright sign

--- inc/footer.php
+++ inc/footer.php
@@ -4,10 +4,10 @@ function copyright($startYear) {
 	$currentYear = date('Y');
 	if ($startYear < $currentYear) {
 		$currentYear = date('y');
-		return "<p><small>&copy Copyright $startYear &ndash; $currentYear" . " | <a id='home_page' href=\"https://visit-neum.com\">visit-neum.com</a><span class='footer_comma'>,</span><span class='footer_none'> | 
+		return "<p><small>&copy; Copyright $startYear &ndash; $currentYear" . " | <a id='home_page' href=\"https://visit-neum.com\">visit-neum.com</a><span class='footer_comma'>,</span><span class='footer_none'> | 
 		</span><span class='footer_block'>Developed by <a id='copyright_info' href=\"https://mirnesglamocic.com\" target=\"_blank\">Mirnes Glamočić</a></span></small></p>";
 	} else {
-		return "<p><small>&copy Copyright $startYear " . " | <a id='home_page' href=\"https://visit-neum.com\">visit-neum.com</a><span class='footer_comma'>,</span><span class='footer_none'> | 
+		return "<p><small>&copy; Copyright $startYear " . " | <a id='home_page' href=\"https://visit-neum.com\">visit-neum.com</a><span class='footer_comma'>,</span><span class='footer_none'> | 
 		</span><span class='footer_block'>Developed by <a id='copyright_info' href=\"https://mirnesglamocic.com\" target=\"_blank\">Mirnes Glamočić</a></span></small></p>";
 	}
 }
