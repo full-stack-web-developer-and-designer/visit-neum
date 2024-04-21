898c8cec4eb5cd9d91cc06988eb3e7d9d24e8d8c
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-21 22:52:06 +0900

    Added my contact phone for contact me

--- inc/nav_moduls/nav_right_nav.php
+++ inc/nav_moduls/nav_right_nav.php
@@ -6,8 +6,10 @@
 	</div><!-- end .modal_heading-->
 		<div class="modal-body">
 			<div id="intro">
-				<p>Poštovani,</p><p>Razvoj interneta je u sve većem toku, te je gotovo nezamislivo zamisliti osobu koji ne koristi internet u današnja vremena. Samim tim, danas mnogi od nas, sve što požele nabavljaju iz udobnosti svog doma, jer to su mogućnosti 21. vijeka, koje nam internet pruža.</p><p>Stoga, imajući to u vidu, sasvim je jasno, da će i uslugu koju Vi nudite, ljudi odlučiti potražiti putem interneta, a ukoliko Vas ne pronađu tamo, gubite velike šanse da budete nečiji potencijalni izbor. Zato, ne oklijevajte, predložite nam Vašu uslugu već danas i <strong>za samo 100KM godišnje postanite dostupni širom svijeta.</strong> Također, ne zaboravite, najbolja investicija je ulaganje u sebe, a ovo je investicija koja će Vam se zagarantovano višestruko isplatiti kroz vrlo kratko vrijeme.</p>
-				<p>Za sva dodatna pitanja ne oklijevajte da nas kontaktirate!</p>
+				<p>Poštovani,</p>
+				<p>Razvoj interneta je u sve većem toku, te je gotovo nezamislivo zamisliti osobu koji ne koristi internet u današnja vremena. Samim tim, danas mnogi od nas, sve što požele nabavljaju iz udobnosti svog doma, jer to su mogućnosti 21. vijeka, koje nam internet pruža.</p>
+				<p>Stoga, imajući to u vidu, sasvim je jasno, da će i uslugu koju Vi nudite, ljudi odlučiti potražiti putem interneta, a ukoliko Vas ne pronađu tamo, gubite velike šanse da budete nečiji potencijalni izbor. Zato, ne oklijevajte, predložite nam Vašu uslugu već danas i <strong>za samo 100KM godišnje postanite dostupni širom svijeta.</strong> Također, ne zaboravite, najbolja investicija je ulaganje u sebe, a ovo je investicija koja će Vam se zagarantovano višestruko isplatiti kroz vrlo kratko vrijeme.</p>
+				<p id="contact_tel">Za sva dodatna pitanja ne oklijevajte da nas kontaktirate putem formulara ili pozivom na broj <a href="tel:+38763734327">+387 63 734 327</a></p>
 				<p class = "sign">Vaš visit-neum.com</p>
 			</div><!-- end #intro-->
 			<div id="ad">
